96636254df1d4471e9635f2f0cdf4eda0a688e82
gt <jack139@gmail.com>
2024-02-08 16:07:34 +0800

    update name dataset

--- convert_names.py
+++ convert_names.py
@@ -1,10 +1,16 @@
+# famous people names
+# more than two words
+
 import pandas as pd
 import json
 
-df=pd.read_csv("data/external/pantheon.tsv", sep='\t')
+# https://pantheon.world/data/datasets
+df=pd.read_csv("data/external/person_2020_update.csv", dtype=str)
 d = df["name"].tolist()
-b = [i for i in d if len(i.split())>1]
-b += ['Jeff Bezos']
+
+d = [i.split(",")[0] for i in d]
+b = [i.lower() for i in d if len(i.split())>1]
+
 
 json.dump(
     b,
