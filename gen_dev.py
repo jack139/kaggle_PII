b7d8f3db9c4c323ea10fd42017471d2cadf5b88b
gt <jack139@gmail.com>
2024-02-11 20:31:46 +0800

    add submission data in gen_dev

--- gen_dev.py
+++ gen_dev.py
@@ -32,6 +32,15 @@ for n, l in tqdm(enumerate(data)):
     if (l['document'] in test_doc) or (n<split_n):
     	dev_data.append(l)
 
+for x in dev_data:
+	x['submission'] = []
+	for n, i in enumerate(x['labels']):
+		if i!='O':
+			x['submission'].append((n, i))
+
+	if x['document'] in test_doc:
+		print('\n'.join([f"{x['document']}, {i[0]}, {i[1]}" for i in x['submission']]))
+
 json.dump(
     dev_data,
     open("data/gen_dev.json", 'w', encoding='utf-8'),
@@ -41,3 +50,42 @@ json.dump(
 
 print("test document: ", test_doc)
 print("dev_n= ", len(dev_data))
+
+
+'''
+7, 9, B-NAME_STUDENT
+7, 10, I-NAME_STUDENT
+7, 482, B-NAME_STUDENT
+7, 483, I-NAME_STUDENT
+7, 741, B-NAME_STUDENT
+7, 742, I-NAME_STUDENT
+
+10, 0, B-NAME_STUDENT
+10, 1, I-NAME_STUDENT
+10, 464, B-NAME_STUDENT
+10, 465, I-NAME_STUDENT
+
+16, 4, B-NAME_STUDENT
+16, 5, I-NAME_STUDENT
+
+20, 5, B-NAME_STUDENT
+20, 6, I-NAME_STUDENT
+
+56, 12, B-NAME_STUDENT
+56, 13, I-NAME_STUDENT
+
+86, 6, B-NAME_STUDENT
+86, 7, I-NAME_STUDENT
+
+93, 0, B-NAME_STUDENT
+93, 1, I-NAME_STUDENT
+
+104, 8, B-NAME_STUDENT
+104, 9, I-NAME_STUDENT
+
+112, 5, B-NAME_STUDENT
+112, 6, I-NAME_STUDENT
+
+123, 32, B-NAME_STUDENT
+123, 33, I-NAME_STUDENT
+'''
